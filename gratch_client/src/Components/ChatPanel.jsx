d78060f34b2f654e19f0d3738c01502984053e14
corax <abubekera87@gmail.com>
2024-05-28 17:23:14 +0300

    fixed heigh issues in some components

--- gratch_client/src/Components/ChatPanel.jsx
+++ gratch_client/src/Components/ChatPanel.jsx
@@ -17,7 +17,7 @@ const ChatPanel = ({ io, roomId }) => {
   }, [messages]);
 
   return (
-    <div className="flex flex-col w-1/3 border-2 border-t-0 border-black border-b-0 ">
+    <div className="flex flex-col w-1/3 border-2 border-t-0 border-black border-b-0  h-screen">
       <div className="bg-bluee h-20 text-white border-b-2 border-black shadow-2xl ">
         <span className="flex justify-center p-5 text-3xl">Chat</span>
       </div>
--- gratch_client/src/Pages/LoginPage.jsx
+++ gratch_client/src/Pages/LoginPage.jsx
@@ -13,26 +13,28 @@ const LoginPage = () => {
   };
   return (
     <div className="h-screen w-screen relative">
-      <div className=" absolute  text-center w-full">
-        <div className=" h-96 w-96 m-auto  mt-96  pt-24 ">
-          <form action="" className="flex flex-col " onSubmit={handleSubmit}>
-            <input
-              type="text"
-              className=" h-14 rounded-xl opacity-50 bg-pink-200 p-4 text-xl  font-semibold  shadow-2xl  mt-4 "
-              placeholder="Enter a Username"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <button
-              className={
-                " bg-pink-600 h-12  w-44 m-auto rounded-xl text-2xl  text-white   shadow-pink-700 shadow-2xl mt-12"
-              }
-            >
-              LOGIN
-            </button>
-          </form>
+      <div className=" absolute h-full text-center w-full ">
+        <div className=" h-full w-full flex items-center">
+          <div className=" h-96 w-96 m-auto    pt-24 ">
+            <form action="" className="flex flex-col " onSubmit={handleSubmit}>
+              <input
+                type="text"
+                className=" h-14 rounded-xl opacity-50 bg-pink-200 p-4 text-xl  font-semibold  shadow-2xl  mt-4 "
+                placeholder="Enter a Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <button
+                className={
+                  " bg-pink-600 h-12  w-44 m-auto rounded-xl text-2xl  text-white font-semibold    shadow-pink-700 shadow-2xl mt-12"
+                }
+              >
+                LOGIN
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-      <img src={background} alt="" className="h-screen w-screen static" />
+      <img src={background} alt="" className=" h-screen w-screen static" />
     </div>
   );
 };
